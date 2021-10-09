584f5a5f3cb840bf7d2698b2024bdf7648c6441a
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-10-08 23:14:17 -0400

    Create NavBar component structure

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,1 +1,23 @@
 import 'navBar.css'
+import { NavLink } from 'react-router-dom'
+
+import './.css'
+
+export default function NavBar () {
+  return (
+    <div className='navbar'>
+      <NavLink activeClassName='active' to='#' exact>
+        Home
+      </NavLink>
+      <NavLink activeClassName='active' to='#' exact>
+        User List
+      </NavLink>
+      <NavLink activeClassName='active' to='#' exact>
+        Address List
+      </NavLink>
+      <NavLink activeClassName='active' to='/404'>
+        Error 404: Page not found.
+      </NavLink>
+    </div>
+  )
+}
