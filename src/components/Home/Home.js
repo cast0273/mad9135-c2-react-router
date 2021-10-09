1a631e4d12a632ab0f68ea7c85cf83e15a98d84c
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-10-09 18:41:42 -0400

    add onClick event to Home component for fetchData

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,15 @@
 import './home.css'
 
-export default function Home () {
-  return <div className='Home'>Home</div>
+export default function Home (props) {
+  function handleClick () {
+    props.fetchData()
+  }
+
+  return (
+    <div className='Home'>
+      <h1>Home</h1>
+      <h2>Click on the button to load user list data.</h2>
+      <p onClick={handleClick}>Get my User List now!</p>
+    </div>
+  )
 }
