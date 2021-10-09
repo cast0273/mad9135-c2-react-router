be37dd4d9cffd11613497b7168fd1ef6b8bc3be1
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-10-08 23:39:49 -0400

    add NavBar links and respective paths

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,13 +6,11 @@ import './.css'
 export default function NavBar () {
   return (
     <div className='NavBar'>
-      <NavLink activeClassName='active' to='/' exact>
-        Home
-      </NavLink>
-      <NavLink activeClassName='active' to='#' exact>
+      <NavLink activeClassName='active' to='/' exact />
+      <NavLink activeClassName='active' to='/userList' exact>
         User List
       </NavLink>
-      <NavLink activeClassName='active' to='#' exact>
+      <NavLink activeClassName='active' to='/userAddress' exact>
         Address List
       </NavLink>
       <NavLink activeClassName='active' to='/404'>
