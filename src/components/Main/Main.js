55d6cd9e044f8cb47d9a94030b345a4ccd14dd7f
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-10-08 23:32:55 -0400

    create HTML structure of Main. Add Switch, Redirect and Route modules to their respective page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,31 @@
 import './main.css'
 
+import UserList from '../UserList/UserList'
+import UserAddress from '../UserAddress/UserAddress'
+import Error from '../Error/Error'
+
+import { Switch, Route, Redirect } from 'react-router-dom'
+
 export default function Main () {
-  return <div>Main</div>
+  return (
+    <div className='Main'>
+      <Switch>
+        <Route path='/'>Instructions</Route>
+
+        <Route path='/userlist'>
+          <UserList />
+        </Route>
+
+        <Route path='/useraddress'>
+          <UserAddress />
+        </Route>
+
+        <Route path='/error'>
+          <Error />
+        </Route>
+
+        <Redirect to='404' />
+      </Switch>
+    </div>
+  )
 }
