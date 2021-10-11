88b32c282ef88fa5e6f90dc05192b5d6dae896cb
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-10-11 00:27:45 -0400

    update button styling

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,5 +1,31 @@
 import './userList.css'
 
-export default function UserList () {
-  return <div className='UserList'>User list</div>
+import { useEffect } from 'react'
+
+export default function UserList ({ data }) {
+  console.log(data)
+
+  useEffect(() => {
+    console.log('useEffect was called.')
+  }, [data])
+
+  return (
+    <div className='UserList'>
+      {data.length === 0 && <p>Loading...</p>}
+      <div className='planet-list'>
+        {data.map((item, index) => (
+          <p key={index}>
+            {/* <NavLink to={`/planets/${index + 1}`}>{item.name}</NavLink> */}
+            <span>{item.name.first}</span>
+          </p>
+        ))}
+      </div>
+
+      {/* <div className='planet-details'>
+        <Route path='/planets/:id'>
+          <Planet findPlanet={findPlanet} />
+        </Route>
+      </div> */}
+    </div>
+  )
 }
