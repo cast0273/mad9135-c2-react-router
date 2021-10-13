9d3489d2ab3f3828927b9e652f0ba458bd9f93c8
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-10-13 16:51:43 -0400

    update the logic of User component, now build data from its props

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,55 +1,45 @@
 import './user.css'
 
-import { useParams } from 'react-router'
-import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router'
 
-export default function User (props) {
-  //
-  const { findUser } = props
-  const { id } = useParams()
-
-  const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    console.log('useEffect1 was called.')
-    setUser(findUser(id))
-  }, [findUser, id])
-
-  useEffect(() => {
-    console.log('useEffect2 was called.')
-    console.log(user)
-  }, [user])
+export default function User () {
+  console.log(useLocation())
+  const { state } = useLocation()
+  console.log(state)
 
   return (
     <>
-      <div id={id}>
-        {console.log(user)}
-        {/* {user.length === 0 && <p>Loading...</p>} */}
+      {state === undefined && <p>Loading...</p>}
 
-        {/* <div className='user'>
+      <div>
+        <div className='user'>
           <div className='wrapper'>
-            <img src={user.picture.large} alt='User thumbnail' />
+            <img src={state && state.item.picture.large} alt='User thumbnail' />
             <div className='userDetails'>
-              <p>{`${user.name.first} ${user.name.last}`}</p>
-              <p>{user.email}</p>
-              <p>{user.cell}</p>
+              <p>{`${state && state.item.name.first} ${state &&
+                state.item.name.last}`}</p>
+              <p>{state && state.item.email}</p>
+              <p>{state && state.item.cell}</p>
             </div>
           </div>
-        </div> */}
+        </div>
 
-        {/* <div className='location'>
-          <h3>Address</h3>
-          <p>{user.location.street}</p>
-          <p>{`${user.location.city}, ${user.location.state}`}</p>
-          <p>{user.location.postcode}</p>
-        </div> */}
+        <div className='userMoreDetails'>
+          <div className='location'>
+            <h3>Address</h3>
+            <p>{state && state.item.location.street.name}</p>
+            <p>{`${state && state.item.location.city}, ${state &&
+              state.item.location.state}`}</p>
+            <p>{state && state.item.location.postcode}</p>
+          </div>
 
-        {/* <div className='login'>
-          <h3>Login</h3>
-          <p>{user.login.uuid}</p>
-          <p>{user.login.username}</p>
-          <p>{user.login.password}</p>
-        </div> */}
+          <div className='login'>
+            <h3>Login</h3>
+            <p>UserID: {state && state.item.login.uuid}</p>
+            <p>Username: {state && state.item.login.username}</p>
+            <p>Password: {state && state.item.login.password}</p>
+          </div>
+        </div>
       </div>
     </>
   )
